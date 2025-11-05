7db25fd2c90343ea34262dc9dae012354fc5fad2
mitrofonov.konstantin@mail.ru <sssscccc462@gmail.com>
2025-11-05 11:42:14 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,12 @@
 ?>
 <footer class="footer">
     <div class="footer-links">
-        <a href="Contact Us.php" class="footer-link">Contact Us</a>
+        <a href="contact_us.php" class="footer-link">Contact Us</a>
         <a href="DMCA.php" class="footer-link">DMCA</a>
-        <a href="Cookie Policy.php" class="footer-link">Cookie Policy</a>
-        <a href="Privacy Policy.php" class="footer-link">Privacy Policy</a>
+        <a href="cookie_policy.php" class="footer-link">Cookie Policy</a>
+        <a href="privacy_policy.php" class="footer-link">Privacy Policy</a>
     </div>
 </footer>
 </body>
+
 </html>
